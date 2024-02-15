b4c4e8f8b8d302a7f5d287e4f1169021491f16c8
ninthevencry <vince.henry@me.com>
2024-02-15 16:04:06 +0000

    Javascript stuff

--- myTest.js
+++ myTest.js
@@ -1,3 +1,64 @@
 console.log("I’m learning about");
 setTimeout(() => { console.log("Event Loop"); }, 2000);
 console.log("the");
+
+const canIVote = age => {
+    /*  if (age >= 18) {
+        return true;
+      } else {
+        return false;
+      }*/
+      //example of conditional operator
+      return age >= 18 ? true : false;
+    }
+    
+    // Uncomment the line below when you're ready to try out your function
+     console.log(canIVote(10)) // Should print true
+
+     const agreeOrDisagree = (string1, string2) => {
+        return string1 === string2 ? "You agree!" : "You disagree!";
+      }
+      
+      // Uncomment the line below when you're ready to try out your function
+      console.log(agreeOrDisagree("yep", "yep")) 
+
+    const calculateWeight = (earthWeight, planet) => {
+        let data = ['mercury',0.378,'venus',0.907,'mars',0.377,'jupiter',2.36,'saturn',0.916];
+        let idx = data.indexOf(planet.toLowerCase());
+        if (idx === 0) {
+            return "Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.";
+        } else {
+            return earthWeight * data[idx + 1];
+        }      
+    }
+
+    const truthyOrFalsy = (myval) => { 
+        return myval ? true : false;
+      }
+
+      const isEven = (n) => { 
+        return n % 2 == 0 ? true : false;
+      }
+    
+      const howOld = (age, year) => {
+        let today = new Date();
+        const currentYear = today.getFullYear();
+        const birthYear = currentYear - age;
+        if (year > currentYear) {
+          let newAge = age + (year - currentYear);
+          return `You will be ${newAge} in the year ${year}`;
+        }
+        if (year < birthYear) {
+          let numYears = birthYear - year;
+          return `The year ${year} was ${numYears} years before you were born`;
+        }
+        if (year > birthYear && year < currentYear) {
+          let newAge = year - birthYear;  
+          return `You were ${newAge} in the year ${year}`;
+        }
+        if (year === currentYear) {
+            return `You are ${age} in the year ${year}`;    
+        }        
+      }
+      
+      console.log(howOld(51,1965));      
--- working.js
+++ working.js
@@ -3,10 +3,12 @@ const average = array => array.reduce((a, b) => a + b) / array.length;
 let myarray = [1, 4, 4, 3, 4, 6, 7, 7, 20];
 console.log(average(myarray));
 */
+/*
 const prompt=require("prompt-sync")({sigint:true}); 
 
 let name = prompt("What's your name");
 console.log("hello"+name+"!");
+*/
 /*
 const getUserChoice = (userInput) => {
     console.log(userInput);
@@ -14,3 +16,38 @@ const getUserChoice = (userInput) => {
   let myChoice = prompt('enter your choice');
   getUserChoice(myChoice);
   */
+
+  let humanScore = 0;
+  let computerScore = 0;
+  let currentRoundNumber = 1;
+  
+  // Write your code below:
+  const generateTarget = () => {
+    return Math.floor(Math.random() * 9);
+  }
+  
+  const compareGuesses = (humanGuess, computerGuess, targetNum) => {
+    console.log(Math.abs(humanGuess - targetNum));
+    console.log(Math.abs(computerGuess - targetNum));
+    if (humanGuess === computerGuess) {
+      return true;
+    } else if (Math.abs(humanGuess - targetNum) < Math.abs(computerGuess - targetNum)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  const updateScore = (winner) => {
+    if (winner == "human") {
+      humanScore += 1;
+    } else {
+      computerScore += 1;
+    }
+  }
+  
+  const advanceRound = () => {
+    currentRoundNumber += 1;
+  }  
+
+  console.log(compareGuesses(0,1,3));
